c9a2235183e92a7708a246034fc77e57e524eef9
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2022-10-01 13:34:42 -0400

    Time: 69 ms (15.88%), Space: 13.9 MB (71.14%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        q=deque([root])
+        stack=[]
+        while q:
+            for i in range(len(q)):
+                Node=q.popleft()
+                if Node and Node.left:
+                    q.append(Node.left)
+                if Node and Node.right:
+                    q.append(Node.right)
+            if Node:
+                stack.append(Node.val)
+        return stack
+        
