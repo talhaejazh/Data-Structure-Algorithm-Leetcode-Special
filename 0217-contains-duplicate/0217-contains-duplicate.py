052d6435bec93da470318f7f5b89ecdfd1b8fd5c
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2022-11-02 18:38:25 -0400

    Time: 1165 ms (29.26%), Space: 26 MB (67.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,6 +4,45 @@ class Solution:
         for i in nums:
             if i not in hashMap:
                 hashMap[i]=1
+            else:
+                return True
+        return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        hashMap={}
+        for i in nums:
+            if i not in hashMap:
+                hashMap[i]=1
 
             else:
                 return True
@@ -13,11 +52,3 @@ class Solution:
     
         
         
-#         hmap = {}
-
-#         for num in nums:
-#             if num not in hmap:
-#                 hmap[num] = 1
-#             else:
-#                 return True
-#         return False
