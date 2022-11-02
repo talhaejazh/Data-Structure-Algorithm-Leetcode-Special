5cc0f410daf2213254bed11022a073ac76661c67
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2022-11-02 18:34:38 -0400

    Time: 63 ms (81.77%), Space: 14.5 MB (35.30%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len (s) != len (t):
+            return False
+        hashMapS={}
+        hashMapT={}
+        for i in range(len(s)):
+            hashMapS[s[i]]=1 + hashMapS.get(s[i],0)
+            hashMapT[t[i]]=1 + hashMapT.get(t[i],0)    
+            # print(hashMapS)
+            # print(hashMapT)
+        for j in hashMapS:
+            if hashMapS[j]!=hashMapT.get(j,0):
+                return False
+        else:
+            return True
+        
