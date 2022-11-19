43197a7378e6a44e5fcc864ff89613d878881201
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2022-11-19 00:47:57 -0500

    Time: 607 ms (76.65%), Space: 26 MB (68.37%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,9 +4,35 @@ class Solution:
         for i in nums:
             if i not in hashMap:
                 hashMap[i]=1
+
             else:
                 return True
         return False
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         hashMap={}
+#         for i in nums:
+#             if i not in hashMap:
+#                 hashMap[i]=1
+#             else:
+#                 return True
+#         return False
         
         
         
