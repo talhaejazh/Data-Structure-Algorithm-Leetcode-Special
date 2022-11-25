2d827919d68b42d7ff556cc627c5a743180e5910
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2022-11-25 17:32:42 -0500

    Time: 134 ms (80.28%), Space: 18.4 MB (39.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,35 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         hashMap=dedfaultfict()
-#         for str in strs:
-#             count=[0] 
-        letters_to_words = defaultdict(list)
+        hashMap=defaultdict(list)
         for word in strs:
-            letters_to_words[tuple(sorted(word))].append(word)
-        return list(letters_to_words.values())
+            hashMap[tuple(sorted(word))].append(word)
+            # print(hashMap,hashMap.values())
+        return(hashMap.values())
+
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         letters_to_words = defaultdict(list)
+#         for word in strs:
+#             print(word)
+#             letters_to_words[tuple(sorted(word))].append(word)
+#             print(letters_to_words.values())
+#         return list(letters_to_words.values())
+    
+    
+    
+    #mlogm*n
