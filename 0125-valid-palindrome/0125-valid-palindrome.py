fbeac2099be5ee66bae273f67a22210309583e19
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2022-12-07 23:56:03 -0500

    Time: 46 ms (94.81%), Space: 14.6 MB (45.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l,r=0,len(s)-1
-        while (l<r):
+        while(l<r):
             while(l<r) and not s[l].isalnum():
                 l+=1
             while(l<r) and not s[r].isalnum():
@@ -11,3 +11,27 @@ class Solution:
             l+=1
             r-=1
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # while (l<r):
+        #     while(l<r) and not s[l].isalnum():
+        #         l+=1
+        #     while(l<r) and not s[r].isalnum():
+        #         r-=1
+        #     if s[l].lower()!=s[r].lower():
+        #         return False
+        #     l+=1
+        #     r-=1
+        # return True
