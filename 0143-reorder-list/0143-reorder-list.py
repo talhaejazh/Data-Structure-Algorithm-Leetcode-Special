f4f62e26947c4cdd1b5b17ddc38376518815f6b8
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-01-20 20:06:46 -0500

    Time: 100 ms (53.96%), Space: 23.9 MB (61.51%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,28 +5,77 @@
 #         self.next = next
 class Solution(object):
     def reorderList(self, head):
-        list1=head
-        slow=head
-        fast=head.next
+        #find middle node
+        #set slow and fast pointer
+        slow,fast=head,head.next
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
-        list2=slow.next
+        
+        #reverse the second half
+        #to reverse we need three things
+        prev,cur=None,slow.next #second half 
+        while cur:
+            temp=cur.next
+            cur.next=prev
+            prev=cur
+            cur=temp
         slow.next=None
-        
-        prev=None
-        while list2:
-            nxt=list2.next
-            list2.next=prev
-            prev=list2
-            list2=nxt
-        list2=prev
-        
-        while list1 and list2:
-            temp1=list1.next
-            temp2=list2.next
-            list1.next=list2
-            list2.next=temp1
-            list1=temp1
-            list2=temp2
+        head1,head2=head,prev
+        while head2:
+            temp=head1.next
+            head1.next=head2
+            head1=head2
+            head2=temp
         return head
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         list1=head
+#         slow=head
+#         fast=head.next
+#         while fast and fast.next:
+#             slow=slow.next
+#             fast=fast.next.next
+#         list2=slow.next
+#         slow.next=None
+        
+#         prev=None
+#         while list2:
+#             nxt=list2.next
+#             list2.next=prev
+#             prev=list2
+#             list2=nxt
+#         list2=prev
+        
+#         while list1 and list2:
+#             temp1=list1.next
+#             temp2=list2.next
+#             list1.next=list2
+#             list2.next=temp1
+#             list1=temp1
+#             list2=temp2
+#         return head
