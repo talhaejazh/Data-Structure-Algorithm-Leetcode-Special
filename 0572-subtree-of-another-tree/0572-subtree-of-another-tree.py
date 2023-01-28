676a4014a5f92e292fbfca6f973ff71bbbdebc1e
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-01-28 18:47:59 -0500

    Time: 121 ms (81.99%), Space: 15.3 MB (10.72%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -5,88 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-#     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-#         if not subRoot:
-#             return True
-#         if not root:
-#             return False
-#         def checkTree(root1,root2):
-#             if not root1 and not root2:
-#                 return True
-#             elif root1 and not root2 or root2 and not root1:
-#                 return False
-#             if root1.val != root1.val:
-#                 return False
-#             return checkTree(root1.left,root2.left) and checkTree(root1.right,root2.right)
-#         def dfs(s,t):
-#             if not s:
-#                 return False
-#             if s.val == t.val and checkTree(s,t):
-#                 return True
-#             return dfs(s.left,t) or dfs(s.right,t)
-#         return dfs(root,subRoot)
-            
-                                                                   
-            
-        
-
-# class Solution(object):
-#     def isSubtree(self, a, b):
-#         if not b:
-#             return True
-        
-#         def checkTree(root1, root2):
-#             if not root1 and not root2:
-#                 return True
-#             elif root1 and not root2 or root2 and not root1:
-#                 return False
-            
-#             if root1.val != root2.val:
-#                 return False
-            
-#             return checkTree(root1.left, root2.left) and checkTree(root1.right, root2.right)
-        
-#         def dfs(s, t):
-#             if not s:
-#                 return False
-            
-#             if s.val == t.val and checkTree(s, t):
-#                 return True
-            
-#             return dfs(s.left, t) or dfs(s.right, t)
-            
-#         return dfs(a, b)  
-
-    def isSubtree(self, root, subRoot):
-        if not subRoot: return True
-        if not root: return False
-        if (self.isSame(root, subRoot)):
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
             return True
-        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-                    
-    def isSame(self, root, subRoot):
-        if not root and not subRoot: return True
-        if not root or not subRoot: return False
-        if root.val == subRoot.val:
-            return(self.isSame(root.left, subRoot.left) and self.isSame(root.right, subRoot.right))
-        return False
+        if not root:
+            return False
+        
+        
+        def dfs(root, subRoot):
+            if not root and not subRoot:
+                return True
+            if root and subRoot and root.val ==subRoot.val:
+                return (dfs(root.left, subRoot.left) and dfs(root.right, subRoot.right))
+            return False
+        if dfs(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))        
+        # if not subRoot: return True
+        # if not root: return False
+        # def dfs(root, subRoot):
+        #     if not root and not subRoot:
+        #         return True
+        #     if root and subRoot and root.val ==subRoot.val:
+        #         return (dfs(root.left, subRoot.left) and dfs(root.right, subRoot.right))
+        #     return False
+        #     if dfs(root, subRoot):
+        #         return True
+        # return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
 
 
 
 
         
-        
-#         def isSame(s,t):
-#             if not s and not t:
-#                 return True
-#             if not s or not t:
-#                 return False
-#             if s.val != t.val:
-#                 return False
-#             return (isSame(s.left,t.left) and isSame(s.right,t.right))
-#             if not subRoot:
-#                 return True
-#             if not root:
-#                 return False
-#         dfs(root,subRoot)
-#             return (isSame(root,subRoot) or self.isSubtree(root.left,subRoot) or                                          self.isSubtree(root.right,subRoot))
