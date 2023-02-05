3617f0b606b82b546aae48719ad2ad2fa8fff96d
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-05 13:33:33 -0500

    Time: 128 ms (87.33%), Space: 15.1 MB (23.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,17 +1,37 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jumps = 0
-        lastPos = 0
-        left = right = 0
-        while right < len(nums) - 1:
-            for i in range(left, right + 1):
-                lastPos = max(lastPos, i + nums[i])
-            left = right + 1
-            right = lastPos
-            jumps += 1
-
+        jumps=0
+        N=len(nums)
+        lastPos=0
+        l=r=0
+        while r<N-1:
+            for i in range(l,r+1):
+                lastPos=max(lastPos,i+nums[i])
+            l=r+1
+            r=lastPos
+            jumps+=1
         return jumps
+            
+        
+#         jumps = 0
+#         lastPos = 0
+#         left = right = 0
+#         while right < len(nums) - 1:
+#             for i in range(left, right + 1):
+#                 lastPos = max(lastPos, i + nums[i])
+#             left = right + 1
+#             right = lastPos
+#             jumps += 1
 
+#         return jumps
+        
+        
+        
+        
+        
+
+# Time: O(N), where N <= 10^4 is the length of array nums
+# Space: O(1)
     
 # Greedy
 
