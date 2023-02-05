0342a605aa9af000e9efb2dd3a65d5f903e90c66
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-05 14:28:33 -0500

    Time: 145 ms (87.98%), Space: 14.5 MB (46.78%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -12,19 +12,30 @@ class Solution:
 #             min=max
 #         return open
                 
-        max_range = [0] * (n + 1)
+#         max_range = [0] * (n + 1)
         
+#         for i, r in enumerate(ranges):
+#             left, right = max(0, i - r), min(n, i + r)
+#             max_range[left] = max(max_range[left], right - left)
+        
+# 		# it's a jump game now
+#         start = end = step = 0
+        
+#         while end < n:
+#             step += 1
+#             start, end = end, max(i + max_range[i] for i in range(start, end + 1))
+#             if start == end:
+#                 return -1
+            
+#         return step       
+    
+        max_range = [0] * (n + 1)
         for i, r in enumerate(ranges):
             left, right = max(0, i - r), min(n, i + r)
-            max_range[left] = max(max_range[left], right - left)
-        
-		# it's a jump game now
+            max_range[left] = max(max_range[left], right)
         start = end = step = 0
-        
         while end < n:
             step += 1
-            start, end = end, max(i + max_range[i] for i in range(start, end + 1))
-            if start == end:
-                return -1
-            
-        return step       
+            start, end = end, max(max_range[i] for i in range(start, end + 1))
+            if start == end: return -1
+        return step
