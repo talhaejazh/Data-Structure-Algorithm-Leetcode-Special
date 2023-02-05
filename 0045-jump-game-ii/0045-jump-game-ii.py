beb0f561b97927a19406a841112b65a864941225
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-05 13:10:44 -0500

    Time: 123 ms (94.85%), Space: 15 MB (55.42%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,22 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         jumps = 0
-        farthest = 0
+        lastPos = 0
         left = right = 0
         while right < len(nums) - 1:
             for i in range(left, right + 1):
-                farthest = max(farthest, i + nums[i])
+                lastPos = max(lastPos, i + nums[i])
             left = right + 1
-            right = farthest
+            right = lastPos
             jumps += 1
 
         return jumps
+
+    
+# Greedy
+
+# The main idea is based on greedy.
+# Step 1: Let's say the range of the current jump is [left, right], farthest is the farthest position 
+# that all positions in [left, right] can reach.
+# Step 2: Once we reach to right, we trigger another jump with left = right + 1, right = farthest, 
+# then repeat step 1 util we reach at the end.
