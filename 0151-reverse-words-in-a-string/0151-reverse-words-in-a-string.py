8c97488ac050cf73ac4204ddd576f2602dd2f9f9
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-05 23:18:08 -0500

    Time: 45 ms (35.17%), Space: 14 MB (74.08%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -3,6 +3,21 @@ class Solution:
        
         result=''
         i,n=0,len(s)
+        while i<n:
+            while i<n and s[i]==' ':
+                i+=1
+            if i>=n:
+                break
+            j=i+1
+            while j<n and s[j]!=' ':
+                j+=1
+            sub=s[i:j]
+            if len(result)==0:
+                result = sub
+            else:
+                result=sub+" "+result
+            i=j+1
+        return result
         # while i<n:
         #     while i<n and s[i]==' ': #find word till space 1 place of word found 
         #         i+1
@@ -22,22 +37,7 @@ class Solution:
         
         
         
-        
-        while i<n:
-            while i<n and s[i]==' ':
-                i+=1
-            if i>=n:
-                break
-            j=i+1
-            while j<n and s[j]!=' ':
-                j+=1
-            sub=s[i:j]
-            if len(result)==0:
-                result = sub
-            else:
-                result=sub+" "+result
-            i=j+1
-        return result
+
 
 ######################method 2 using fucntion only     
 #         return " ".join(reversed(s.split()))             
