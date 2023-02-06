addd3ffd1ce1e77bac27d22b6bca240eed1c5fdd
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-05 23:17:34 -0500

    Time: 34 ms (73.13%), Space: 14.1 MB (39.81%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,23 +1,44 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
        
-#         result=''
-#         i,n=0,len(s)
-#         while i<n:
-#             while i<n and s[i]==' ':
-#                 i+=1
-#             if i>=n:
-#                 break
-#             j=i+1
-#             while j<n and s[j]!=' ':
-#                 j+=1
-#             sub=s[i:j]
-#             if len(result)==0:
-#                 result = sub
-#             else:
-#                 result=sub+" "+result
-#             i=j+1
-#         return result
+        result=''
+        i,n=0,len(s)
+        # while i<n:
+        #     while i<n and s[i]==' ': #find word till space 1 place of word found 
+        #         i+1
+        #     if i>=n:
+        #         break
+        #     j=i+1
+        #     while j<n and s[j]!=' ': #move next pointer till word end
+        #         j+=1
+        #     sub=s[i:j] #create sub words
+        #     if len(result)==0:
+        #         result=sub  #if there is only 1 words place it
+        #     else:
+        #         result=sub+" "+result #interchange it
+        #     i=j+1
+        # return result
                 
-        return " ".join(reversed(s.split())) #method 2 using fucntion only              
+        
+        
+        
+        
+        while i<n:
+            while i<n and s[i]==' ':
+                i+=1
+            if i>=n:
+                break
+            j=i+1
+            while j<n and s[j]!=' ':
+                j+=1
+            sub=s[i:j]
+            if len(result)==0:
+                result = sub
+            else:
+                result=sub+" "+result
+            i=j+1
+        return result
+
+######################method 2 using fucntion only     
+#         return " ".join(reversed(s.split()))             
             
