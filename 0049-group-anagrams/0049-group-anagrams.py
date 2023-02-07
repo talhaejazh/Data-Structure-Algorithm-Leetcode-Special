fb427ea3cebfd1c301d8d313fdc43b3a3d768aa9
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-06 23:40:26 -0500

    Time: 91 ms (96.46%), Space: 17.2 MB (78.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-#         hashMap=defaultdict(list)
-#         for cur in strs:
-#             key=''.join(sorted(cur))
-#             if key in hashMap:
-#                 hashMap[key].append(cur)
-#             else:
-#                 hashMap[key]=[cur]
-#         res=[]
-#         for key in hashMap:
-#             res.append(hashMap[key])
-#         return res        
-        
-        
         lookup=defaultdict(list)
         for s in strs:
             key="".join(sorted(list(s)))
