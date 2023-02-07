f1c95636604a7e8a4c67ed28314497e2a3a37238
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-06 22:27:42 -0500

    Time: 78 ms (50.47%), Space: 14.3 MB (5.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,30 +1,30 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        maxLen=0
-        start=0
-        hashmap={}
-        for i,c in enumerate(s):
-            if c in hashmap and start<=hashmap[c]:
-                start=hashmap[c]+1
-            else:
-                maxLen=max(maxLen,i-start+1)
-            hashmap[c]=i
-        return maxLen
+#         maxLen=0
+#         start=0
+#         hashmap={}
+#         for i,c in enumerate(s):
+#             if c in hashmap and start<=hashmap[c]:
+#                 start=hashmap[c]+1
+#             else:
+#                 maxLen=max(maxLen,i-start+1)
+#             hashmap[c]=i
+#         return maxLen
 
         
         
         
         
-#         start=0
-#         maxlen=0    
-#         hashmap={}
-#         for i,c in enumerate(s): #index with characted
-#             if c in hashmap and start<=hashmap[c]: 
-#                 start=hashmap[c]+1 #add value in start if already seen
-#             else:
-#                 maxlen=max(maxlen,i-start+1) #add 1 for range issue
-#             hashmap[c]=i #add valye in hashmap
-#         print(hashmap['c'])
-#         return maxlen
+        start=0
+        maxlen=0    
+        hashmap={}
+        for i,c in enumerate(s): #index with characted
+            if c in hashmap and start<=hashmap[c]: 
+                start=hashmap[c]+1 #add value in start if already seen
+            else:
+                maxlen=max(maxlen,i-start+1) #add 1 for range issue
+            hashmap[c]=i #add valye in hashmap
+            print(hashmap[c])
+        return maxlen
             
         
