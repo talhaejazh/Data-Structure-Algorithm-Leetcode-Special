2cdadb958e5a7d7cff0c5117b85fc94d26fc930e
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-06 21:20:56 -0500

    Time: 45 ms (34.48%), Space: 14 MB (74.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -18,21 +18,21 @@ class Solution:
                 result=sub+" "+result
             i=j+1
         return result
-        # while i<n:
-        #     while i<n and s[i]==' ': #find word till space 1 place of word found 
-        #         i+1
-        #     if i>=n:
-        #         break
-        #     j=i+1
-        #     while j<n and s[j]!=' ': #move next pointer till word end
-        #         j+=1
-        #     sub=s[i:j] #create sub words
-        #     if len(result)==0:
-        #         result=sub  #if there is only 1 words place it
-        #     else:
-        #         result=sub+" "+result #interchange it
-        #     i=j+1
-        # return result
+#         while i<n:
+#             while i<n and s[i]==' ': #find word till space 1 place of word found 
+#                 i+1
+#             if i>=n:
+#                 break
+#             j=i+1
+#             while j<n and s[j]!=' ': #move next pointer till word end
+#                 j+=1
+#             sub=s[i:j] #create sub words
+#             if len(result)==0:
+#                 result=sub  #if there is only 1 words place it
+#             else:
+#                 result=sub+" "+result #interchange it
+#             i=j+1
+#         return result
                 
         
         
