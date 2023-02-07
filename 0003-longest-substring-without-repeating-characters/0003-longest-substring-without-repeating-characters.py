ba572ff0040532956b4a797bab83a22fe0f8f403
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-06 22:18:59 -0500

    Time: 47 ms (98.42%), Space: 14.1 MB (45.61%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,14 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        maxLen=0
         start=0
-        maxlen=0
         hashmap={}
         for i,c in enumerate(s):
             if c in hashmap and start<=hashmap[c]:
                 start=hashmap[c]+1
             else:
-                maxlen=max(maxlen,i-start+1)
+                maxLen=max(maxLen,i-start+1)
             hashmap[c]=i
-        return maxlen
+        return maxLen
+        
+        
+        
+        
+#         start=0
+#         maxlen=0    
+#         hashmap={}
+#         for i,c in enumerate(s): #index with characted
+#             if c in hashmap and start<=hashmap[c]: 
+#                 start=hashmap[c]+1 #add value in start if already seen
+#             else:
+#                 maxlen=max(maxlen,i-start+1) #add 1 for range issue
+#             hashmap[c]=i #add valye in hashmap
+#         print(hashmap['c'])
+#         return maxlen
             
         
