02d3677d9e122f9fc38fc24add4e140ce4c8f35e
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-06 23:49:32 -0500

    Time: 114 ms (52.25%), Space: 17.2 MB (78.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,31 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
+        
         lookup=defaultdict(list)
         for s in strs:
-            key="".join(sorted(list(s)))
+            key="".join(sorted(list(s))) #first list then sort and the join as str [abc,abc,abc]
             if key in lookup:
-                lookup[key].append(s)
+                lookup[key].append(s) # something like this [1(eat,eat,eat),2(abc,abc)]
             else:
                 lookup[key]=[s]
-        output=[] #to create a list intialize empty list
+        output=[]
         for key in lookup:
             output.append(lookup[key])
+            print(lookup[key])
         return output
+        
+        # lookup=defaultdict(list)
+        # for s in strs:
+        #     key="".join(sorted(list(s)))
+        #     if key in lookup:
+        #         lookup[key].append(s)
+        #     else:
+        #         lookup[key]=[s]
+        # output=[] #to create a list intialize empty list
+        # for key in lookup:
+        #     output.append(lookup[key])
+        # return output
         
         
         # return [l for l in lookup.values()]:
