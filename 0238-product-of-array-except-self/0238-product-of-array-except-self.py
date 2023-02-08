972e75b68dfcf855ffbd0e325840dc9acc3bedfa
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-07 23:18:37 -0500

    Time: 244 ms (64.76%), Space: 22.7 MB (12.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -2,18 +2,18 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         l,r = [1]*len(nums),[1]*len(nums)
         #for right array
-        revert=nums[::-1]
+        revert=nums[::-1]  #[4,3,2,1]
         # print(l,r,revert)
         result=[]
         #Build left array
         for i in range(1,len(nums)):
-            l[i]=l[i-1]*nums[i-1]
+            l[i]=l[i-1]*nums[i-1]    #[1,1,2,6] 
         #buld right array
         for i in range(1,len(nums)):
-            r[i]=r[i-1]*revert[i-1]
+            r[i]=r[i-1]*revert[i-1]  #[1,4,12,8]
         
         #Mulitply array in same direction(Flip)
-        x=r[::-1]
+        x=r[::-1]                    #[8,12,4,1]
         for i in range(len(nums)):
             result.append(l[i]*x[i])
         # print(result)
