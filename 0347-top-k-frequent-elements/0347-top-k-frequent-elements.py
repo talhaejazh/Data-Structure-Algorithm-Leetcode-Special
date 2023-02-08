292fed0b4f9e6e3c9879112bca060c1cabf85455
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-07 22:58:28 -0500

    Time: 96 ms (96.42%), Space: 18.7 MB (67.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,15 +7,32 @@ class Solution:
                 freq[num]=1
             else:
                 freq[num]+=1
-        print(freq)
         for key,value in freq.items():
-            print(key,value)
             if len(ans)<k:
-                heapq.heappush(ans,[value,key])
+                heapq.heappush(ans,(value,key))
             else:
-                heapq.heappushpop(ans,[value,key])
-        print(ans)
-        return [key for value,key in ans]
+                heapq.heappushpop(ans,(value,key))
+        return (key for value,key in ans)
+        
+        
+        
+        
+        # ans=[]
+        # freq=dict()
+        # for num in nums:
+        #     if num not in freq:
+        #         freq[num]=1
+        #     else:
+        #         freq[num]+=1
+        # print(freq)
+        # for key,value in freq.items():
+        #     print(key,value)
+        #     if len(ans)<k:
+        #         heapq.heappush(ans,[value,key])
+        #     else:
+        #         heapq.heappushpop(ans,[value,key])
+        # print(ans)
+        # return [key for value,key in ans]
     
     #time nlogk
     #space On
