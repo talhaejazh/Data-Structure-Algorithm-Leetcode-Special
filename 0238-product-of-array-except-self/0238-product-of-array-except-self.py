ad9691548757de0408ac8bb65131fd27ca2c7a56
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-07 23:39:32 -0500

    Time: 293 ms (27.56%), Space: 24.2 MB (5.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,39 +1,43 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        l,r=[1]*n,[1]*n
-        print(l)
-        revert=nums[::-1]
-        print(revert)
-        ##for left partial
-        for i in range(1,n):
-            l[i]=l[i-1]*nums[i-1]
-        ##for right partial
-        for i in range(1,n):
-            r[i]=r[i-1]*revert[i-1]
-        print(l,r)  
+
+        l,r = [1]*len(nums),[1]*len(nums)  #[1,1,1,1]
+        #for right array
+        revert=nums[::-1]  #[4,3,2,1]
+        
         result=[]
-        flip=r[::-1]
+        #Build left array
+        for i in range(1,len(nums)):
+            l[i]=l[i-1]*nums[i-1]    #[1,1,2,6] 
+        #buld right array
+        for i in range(1,len(nums)):
+            r[i]=r[i-1]*revert[i-1]  #[1,4,12,8]
+        print(l,r)
+        
+        #Mulitply array in same direction(Flip)
+        flip=r[::-1]                    #[8,12,4,1]
         print(flip)
-        for i in range(n):
+        for i in range(len(nums)):
             result.append(l[i]*flip[i])
-        return result
+        # print(result)
+        return(result)
+    
+        # n=len(nums)
+        # l,r=[1]*n,[1]*n
+        # print(l)
+        # revert=nums[::-1]
+        # print(revert)
+        # ##for left partial
+        # for i in range(1,n):
+        #     l[i]=l[i-1]*nums[i-1]
+        # ##for right partial
+        # for i in range(1,n):
+        #     r[i]=r[i-1]*revert[i-1]
+        # print(l,r)  
+        # result=[]
+        # flip=r[::-1]
+        # print(flip)
+        # for i in range(n):
+        #     result.append(l[i]*flip[i])
+        # return result
         
-#         l,r = [1]*len(nums),[1]*len(nums)  #[1,1,1,1]
-#         #for right array
-#         revert=nums[::-1]  #[4,3,2,1]
-#         # print(l,r,revert)
-#         result=[]
-#         #Build left array
-#         for i in range(1,len(nums)):
-#             l[i]=l[i-1]*nums[i-1]    #[1,1,2,6] 
-#         #buld right array
-#         for i in range(1,len(nums)):
-#             r[i]=r[i-1]*revert[i-1]  #[1,4,12,8]
-        
-#         #Mulitply array in same direction(Flip)
-#         x=r[::-1]                    #[8,12,4,1]
-#         for i in range(len(nums)):
-#             result.append(l[i]*x[i])
-#         # print(result)
-#         return(result)
