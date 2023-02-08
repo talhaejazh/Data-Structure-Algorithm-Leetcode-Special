a60074f329546a579c3bab160877ccba3052df5c
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-08 17:06:32 -0500

    Time: 771 ms (69.15%), Space: 27.4 MB (46.11%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -29,12 +29,12 @@ class Solution:
         
         
         
-        area,l,r=0,0,len(height)-1
-        while l<r:
-            h=min(height[l],height[r])
-            area=max(area,(r-l)*h)
-            if height[l]<height[r]:
-                l+=1
-            else:
-                r-=1
-        return area
+#         area,l,r=0,0,len(height)-1
+#         while l<r:
+#             h=min(height[l],height[r])
+#             area=max(area,(r-l)*h)
+#             if height[l]<height[r]:
+#                 l+=1
+#             else:
+#                 r-=1
+#         return area
