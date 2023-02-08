a714c6fdb6154572c82338e5a73e65d6abed75b9
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-08 15:32:02 -0500

    Time: 1522 ms (50.01%), Space: 18.3 MB (40.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,11 +6,11 @@ class Solution(object):
         for i in range(len(nums)):
             if i >0 and nums[i]==nums[i-1]:
                 continue
-            curVal=nums[i]
+            # curVal=nums[i]
             left= i+1
             right= len(nums)-1
             while left<right:
-                curSum=curVal+nums[left]+nums[right]
+                curSum=nums[i]+nums[left]+nums[right]
                 if (curSum) ==0:
                     triplets.append([nums[i], nums[left], nums[right]])
                     left+=1
