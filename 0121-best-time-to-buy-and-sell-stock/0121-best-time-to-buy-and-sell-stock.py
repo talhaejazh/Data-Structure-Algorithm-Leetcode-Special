e591226d3725d91434dd08cd0f598214d084ca58
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-08 18:26:04 -0500

    Time: 1111 ms (57.63%), Space: 25.2 MB (5.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,34 +1,34 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        ##2 pointer solutionnstart from one sode
         l=0
         r=1
-        # print(prices[1])
         maxprofit=0
-        while r<len(prices):
-            curpro=prices[r]-prices[l]
+        n=len(prices)
+        while r<n:
+            curprice=prices[r]-prices[l]
             if prices[l]<prices[r]:
-                maxprofit=max(maxprofit,curpro)
+                maxprofit=max(maxprofit,curprice)
             else:
                 l=r
             r+=1
         return maxprofit
-            
         
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        # l=0
+        # r=1
+        # # print(prices[1])
+        # maxprofit=0
+        # while r<len(prices):
+        #     curpro=prices[r]-prices[l]
+        #     if prices[l]<prices[r]:
+        #         maxprofit=max(maxprofit,curpro)
+        #     else:
+        #         l=r
+        #     r+=1
+        # return maxprofit
         
         
 #         left = 0 #Buy
