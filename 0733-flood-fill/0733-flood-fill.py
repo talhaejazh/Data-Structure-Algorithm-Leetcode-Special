6e65c42d7bb1f88851c081e683e8331d8d546a50
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-09 00:52:14 -0500

    Time: 88 ms (29.80%), Space: 14.1 MB (34.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,37 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        row=len(image)
-        cols=len(image[0])
-        direction=[[1,0],[0,1],[-1,0],[0,-1]]
-        visit=set()
-        curCol=image[sr][sc]
+        R,C=len(image),len(image[0])
+        newcolor=image[sr][sc]
+        if newcolor==color:
+            return image
         def dfs(r,c):
-            if r<0 or r>=row or c<0 or c>=cols or (r,c) in visit or image[r][c]!=curCol:
-                return
-            image[r][c]=color
-            visit.add((r,c))
-            for dr,dc in direction:
-                nr=r+dr
-                nc=c+dc
-                dfs(nr,nc)
+            if image[r][c]==newcolor:
+                image[r][c]=color
+                if r>=1: dfs(r-1,c)
+                if r+1<R:dfs(r+1,c)
+                if c>=1: dfs(r,c-1)
+                if c+1<C:dfs(r,c+1)
+        
         dfs(sr,sc)
         return image
+                    
+        
+        
+        
+#         row=len(image)
+#         cols=len(image[0])
+#         direction=[[1,0],[0,1],[-1,0],[0,-1]]
+#         visit=set()
+#         curCol=image[sr][sc]
+#         def dfs(r,c):
+#             if r<0 or r>=row or c<0 or c>=cols or (r,c) in visit or image[r][c]!=curCol:
+#                 return
+#             image[r][c]=color
+#             visit.add((r,c))
+#             for dr,dc in direction:
+#                 nr=r+dr
+#                 nc=c+dc
+#                 dfs(nr,nc)
+#         dfs(sr,sc)
+#         return image
                 
