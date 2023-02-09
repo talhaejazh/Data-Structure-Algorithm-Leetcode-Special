2a33bdce3a3a031aaa38d017a1bcf4b4347fcb05
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-09 16:34:26 -0500

    Time: 34 ms (97.35%), Space: 14.1 MB (93.93%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -10,6 +10,19 @@ class Solution:
         return nums[r]
             
             
+            
+        
+        
+#         l,r=0,len(nums)-1
+#         while l<r:
+#             mid=(l+r)//2
+#             if nums[mid]>nums[r]: #if mid value greater mean pivot is right so we shift l to middle+1
+#                 l=mid+1
+#             else:
+#                 r=mid
+#         return nums[r]
+            
+            
 #         l,r=0,len(nums)-1
 #         while l<r:
 #             mid=(r+l)//2
@@ -18,15 +31,3 @@ class Solution:
 #             else:
 #                 r=mid
 #         return nums[l]
-        
-        
-        
-        
-        # l,r=0,len(nums)-1
-        # while l<r:
-        #     mid=(l+r)//2
-        #     if nums[mid]>nums[r]: 
-        #         l=mid+1
-        #     else:
-        #         r=mid
-        # return nums[l]
