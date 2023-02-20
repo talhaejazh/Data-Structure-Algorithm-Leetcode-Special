5be10e486439d9b26fa99554103c718c24b0fd52
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-20 01:13:22 -0500

    Time: 2981 ms (16.73%), Space: 14 MB (59.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,6 +1,15 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         map1=Counter(s1) #create hashmap directly
+#         for i in range(len(s2)):
+#             subs=s1[i:i+len(s1)]
+#             map2=Counter(subs)
+#             if map1==map2:
+#                 return True
+#         return False
+        
+        
+        
         for i in range(len(s2)):
             subs=s2[i:i+len(s1)] #create sub of 3 string in every itereation
             map2=Counter(subs) #creake hashmap of subs
