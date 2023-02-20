ed965b550b76ef79a9b8e9396ddb69df4c64fe78
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-20 01:08:01 -0500

    Time: 2970 ms (16.78%), Space: 14 MB (59.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,14 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        cnt = Counter(s1)
-        
-        l = 0
-        for r, c in enumerate(s2):
-            cnt[c] -= 1
-            while cnt[c] < 0:  # If number of characters `c` is more than our expectation
-                cnt[s2[l]] += 1  # Slide left until cnt[c] == 0
-                l += 1
-            if r - l + 1 == len(s1):  # If we already filled enough `p.length()` chars
+        map1=Counter(s1)
+        for i in range(len(s2)):
+            subs=s2[i:i+len(s1)]
+            map2=Counter(subs)
+            if map1==map2:
                 return True
+        return False
+        # cnt = Counter(s1)
+        # print(cnt)
+        # l = 0
+        # for r, c in enumerate(s2):
+        #     cnt[c] -= 1
+        #     while cnt[c] < 0:  # If number of characters `c` is more than our expectation
+        #         cnt[s2[l]] += 1  # Slide left until cnt[c] == 0
+        #         l += 1
+        #     if r - l + 1 == len(s1):  # If we already filled enough `p.length()` chars
+        #         return True
             
         return False
