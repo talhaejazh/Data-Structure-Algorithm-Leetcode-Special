5bba76d2caf1b05e738322b1c5cf74c673bb8014
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-02-28 21:02:42 -0500

    Time: 39 ms (50.59%), Space: 14.1 MB (69.43%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -6,10 +6,24 @@ class Solution:
             if left==right==0:
                 output.append(cand)
                 return
-            if left>0:
+            if left>0:   #if we have both ( and ) we first place (
                 rec(left-1,right,stack+1,cand+"(")
-            if right>0 and stack>0:
+            if right>0 and stack>0: #it means we have ) and (  is already in the stack so to close we have to use )
                 rec(left,right-1,stack-1,cand+")")
         rec(n,n,0,"")
         
         return output
+        
+        
+#         output=[]
+#         def rec(left,right,stack,cand):
+#             if left==right==0:
+#                 output.append(cand)
+#                 return
+#             if left>0:
+#                 rec(left-1,right,stack+1,cand+"(")
+#             if right>0 and stack>0:
+#                 rec(left,right-1,stack-1,cand+")")
+#         rec(n,n,0,"")
+        
+#         return output
