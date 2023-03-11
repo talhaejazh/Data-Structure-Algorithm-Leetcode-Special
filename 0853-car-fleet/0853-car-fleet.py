3831db2ae1a516274e5dd1e997449d6a5e4cb1a9
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-03-11 00:54:29 -0500

    Time: 913 ms (86.62%), Space: 36.3 MB (52.64%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,12 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars=sorted(zip(position,speed))
+        cars=sorted(zip(position,speed)) #combine and sorted together [0,1][2,1] 
         n=len(cars)
         ans=[]
-        for i in range(n-1,-1,-1):
-            p,s=cars[i]
-            arrival_time=(target-p)/s
-            if not ans or arrival_time>ans[-1]:
+        for i in range(n-1,-1,-1): # iterated from max speed to lower 
+            p,s=cars[i] #get position and speed from each array
+            arrival_time=(target-p)/s #calculate time
+            if not ans or arrival_time>ans[-1]: #check if they are on same fleet 
                 ans.append(arrival_time)
         return len(ans)
         
