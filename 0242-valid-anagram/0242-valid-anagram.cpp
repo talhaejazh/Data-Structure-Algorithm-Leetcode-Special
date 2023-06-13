0525d12fd0539bd68c5152bc2972f991a00fd5b8
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-06-12 22:42:07 -0400

    Time: 14 ms (47.93%), Space: 7.2 MB (81.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-       unordered_map <char, int> smap,tmap;
-        if (s.length()!= t.length()){
-            return false;
-        }
-        for (int i=0;i<s.length();i++){
-            smap[s[i]]+=1;
-            tmap[t[i]]+=1;
-            
-        }
-        return smap==tmap;
+    bool isAnagram(string s, string t) 
+    { unordered_map <char,int> smap,tmap;
+     if (s.length()!=t.length()){
+         return false;
+     }
+     for (int i=0;i<s.length();i++){
+         smap[s[i]]+=1;
+         tmap[t[i]]+=1;
+     }
+     return smap==tmap;
     }
 };
+//     {
+//        unordered_map <char, int> smap,tmap;
+//         if (s.length()!= t.length()){
+//             return false;
+//         }
+//         for (int i=0;i<s.length();i++){
+//             smap[s[i]]+=1;
+//             tmap[t[i]]+=1;
+            
+//         }
+//         return smap==tmap;
+//     }
+// };
