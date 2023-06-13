bfa98071cfa57dea5448788e9de19c2741ee6a23
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-06-12 20:52:21 -0400

    Time: 145 ms (64.33%), Space: 69.6 MB (46.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> hashset;
+   
         for (int n:nums){
             if (hashset.count(n)>0){
                 return true;
