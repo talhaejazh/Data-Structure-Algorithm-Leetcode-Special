70dec9929d2c02a4ac86bdb55c62c3d0a36c21a4
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-09-27 16:15:54 -0400

    Time: 55 ms (53.91%), Space: 16.6 MB (96.40%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         #Method 1
-#         mydict={}
-#         for l in s:
-#             if l not in mydict:
-#                 mydict[l]=1
-#             else:
-#                 mydict[l]+=1
-#         for l in t:
-#             if l not in mydict:
-#                 return False
-#             else:
-#                 mydict=-1
-#         for value in mydict.values():
-#             if value != 0:
-#                 return False
-#         return True
+        # mydict={}
+        # for char in s:
+        #     if char not in mydict:
+        #         mydict[char]=1
+        #     else:
+        #         mydict[char]+=1
+        # for char in t:
+        #     if char not in mydict:
+        #         return False
+        #     else:
+        #         mydict=-1
+        # for value in mydict.values():
+        #     if value != 0:
+        #         return False
+        # return True
       
         dict_value = {}        
         for letter in s:
