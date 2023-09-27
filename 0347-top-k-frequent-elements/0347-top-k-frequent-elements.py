3e4ff57d993eebea148525e9c56491b354e320d0
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-09-27 19:34:48 -0400

    Time: 96 ms (72.37%), Space: 20.9 MB (81.88%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ans=[]
         freq=defaultdict()
+        ans=[]
         for num in nums:
             if num not in freq:
                 freq[num]=1
@@ -9,30 +9,8 @@ class Solution:
                 freq[num]+=1
         for key,value in freq.items():
             if len(ans)<k:
-                heapq.heappush(ans,(value,key))
+                heapq.heappush(ans,[value,key])
             else:
-                heapq.heappushpop(ans,(value,key))
+                heapq.heappushpop(ans,[value,key])
+        print(ans)
         return (key for value,key in ans)
-        
-        
-        
-        
-        # ans=[]
-        # freq=dict()
-        # for num in nums:
-        #     if num not in freq:
-        #         freq[num]=1
-        #     else:
-        #         freq[num]+=1
-        # print(freq)
-        # for key,value in freq.items():
-        #     print(key,value)
-        #     if len(ans)<k:
-        #         heapq.heappush(ans,[value,key])
-        #     else:
-        #         heapq.heappushpop(ans,[value,key])
-        # print(ans)
-        # return [key for value,key in ans]
-    
-    #time nlogk
-    #space On
