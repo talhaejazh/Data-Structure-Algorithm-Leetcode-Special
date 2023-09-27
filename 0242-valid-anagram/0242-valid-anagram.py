559ce99498329324407d5c5484461ae19ec30ac9
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2023-09-27 16:02:20 -0400

    Time: 54 ms (59.81%), Space: 16.8 MB (80.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len (s) != len (t):
+        if len(t)!=len(s):
             return False
         hashMapS={}
         hashMapT={}
@@ -14,4 +14,3 @@ class Solution:
                 return False
         else:
             return True
-        
