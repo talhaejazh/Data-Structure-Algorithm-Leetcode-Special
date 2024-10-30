495c0480b1bfb0316f79b7ff82a8d0fa48f520e6
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-10-29 22:01:38 -0400

    Time: 3 ms (54.18%), Space: 18.3 MB (6.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,5 +8,17 @@ class Solution:
             else:
                 lookup[target-nums[i]]=i
         return None
+
+        
+        
+        
+        # n=len(nums)
+        # lookup={}
+        # for i in range(n):
+        #     if nums[i] in lookup:
+        #         return [lookup[nums[i]],i]
+        #     else:
+        #         lookup[target-nums[i]]=i
+        # return None
         
         
