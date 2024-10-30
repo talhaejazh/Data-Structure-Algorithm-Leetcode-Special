2096ae2c1cf29b6222e8cef1687e2574016a1659
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-10-29 23:27:56 -0400

    Time: 19 ms (36.41%), Space: 19.8 MB (68.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,23 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         lookup=defaultdict(list)
-        # for s in strs:
-        #     key="". join(sorted(list(s)))
-        #     print(key)
-        # if key in lookup:
-        #     lookup[key].append(s)
-        # else:
-        #     lookup[key]=[s]
-        # output=[]
-        # for key in lookup:
-        #     output.append(lookup[key])
-        #     print (output[key])
-        # return output
-    
         for s in strs:
-            key="".join(sorted(list(s))) #first list then sort and the join as str [abc,abc,abc]
+            key="".join(sorted(list(s)))
             if key in lookup:
-                lookup[key].append(s) # something like this [1(eat,eat,eat),2(abc,abc)]
+                lookup[key].append(s)
             else:
                 lookup[key]=[s]
         output=[]
@@ -25,3 +12,15 @@ class Solution:
             output.append(lookup[key])
             print(lookup[key])
         return output
+    
+        # for s in strs:
+        #     key="".join(sorted(list(s))) #first list then sort and the join as str [abc,abc,abc]
+        #     if key in lookup:
+        #         lookup[key].append(s) # something like this [1(eat,eat,eat),2(abc,abc)]
+        #     else:
+        #         lookup[key]=[s]
+        # output=[]
+        # for key in lookup:
+        #     output.append(lookup[key])
+        #     print(lookup[key])
+        # return output
