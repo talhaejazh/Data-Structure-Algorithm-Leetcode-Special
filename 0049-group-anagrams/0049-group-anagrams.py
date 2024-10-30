1e9581160e5f6d966b3f3e0aa62341a0869ab63b
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-10-29 23:22:11 -0400

    Time: 19 ms (36.41%), Space: 19.9 MB (66.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        
         lookup=defaultdict(list)
+        # for s in strs:
+        #     key="". join(sorted(list(s)))
+        #     print(key)
+        # if key in lookup:
+        #     lookup[key].append(s)
+        # else:
+        #     lookup[key]=[s]
+        # output=[]
+        # for key in lookup:
+        #     output.append(lookup[key])
+        #     print (output[key])
+        # return output
+    
         for s in strs:
             key="".join(sorted(list(s))) #first list then sort and the join as str [abc,abc,abc]
             if key in lookup:
@@ -14,52 +25,3 @@ class Solution:
             output.append(lookup[key])
             print(lookup[key])
         return output
-        
-        # lookup=defaultdict(list)
-        # for s in strs:
-        #     key="".join(sorted(list(s)))
-        #     if key in lookup:
-        #         lookup[key].append(s)
-        #     else:
-        #         lookup[key]=[s]
-        # output=[] #to create a list intialize empty list
-        # for key in lookup:
-        #     output.append(lookup[key])
-        # return output
-        
-        
-        # return [l for l in lookup.values()]:
-        # for l in lookup.values():
-        #     output.append(l):
-        # return output
-            
-        
-        
-        # hashMap=defaultdict(list)
-        # for word in strs:
-        #     hashMap[tuple(sorted(word))].append(word)
-        #     # print(hashMap,hashMap.values())
-        # return(hashMap.values())
-
-            
-        # add=0
-        # mul=1
-        # d=defaultdict(list)
-        # for i in strs:
-        #     for j in i:
-        #         add+=ord(j)
-        #         mul*=ord(j)
-        #     ans=2*add+mul
-        #     d[ans].append(i)
-        #     add=0
-        #     mul=1
-        # return d.values()              
-            
-            
-        
-        
-        
-    
-    
-    
-    #mlogm*n
