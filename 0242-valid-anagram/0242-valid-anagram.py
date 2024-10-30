b73c98be2dbfdcd8539962556135c56f687c0eb0
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-10-29 22:42:51 -0400

    Time: 18 ms (23.23%), Space: 16.7 MB (89.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # If lengths of both strings are not equal, they cannot be anagrams.
-        if len(s) != len(t):
+        if len(s)!=len(t):
             return False
-
-        # Create a dictionary to store character counts.
-        char_count = {}
-
-        # Iterate over the first string and update character counts.
+        char_count={}
         for char in s:
-            char_count[char] = char_count.get(char, 0) + 1
-
-        # Iterate over the second string and decrease character counts.
+            char_count[char]=char_count.get(char,0)+1
+            
+            # print(char_count)
         for char in t:
-            # If a character in t is not in s, it's not an anagram.
             if char not in char_count:
                 return False
-            char_count[char] -= 1
-            # If any character count becomes negative, they aren't anagrams.
-            if char_count[char] < 0:
+            else:
+                char_count[char]-=1
+            if char_count[char]<0:
                 return False
-
         return True
+    
+            
+        
