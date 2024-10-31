7ea7a95dac0108646b16211a90d39d01d52864d5
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-10-30 21:17:13 -0400

    Time: 43 ms (82.11%), Space: 31 MB (93.32%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,33 +1,48 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        output=0
-        if not nums:
+        
+        if len(nums)==0:
             return 0
-        numset=set(nums)
-        print(numset)
-        for n in numset:
-            if n-1 not in numset:
-                start=n
-                while start in numset:
-                    start+=1
-                output=max(output,start-n)
-        return output
+        nums.sort()
+        longest=0
+        curlen=1
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]+1:
+                curlen+=1
+            elif nums[i]!=nums[i-1]:
+                longest=max(curlen,longest)
+                curlen=1
+        return max(curlen,longest)
+                
         
         
+        
+#         if len(nums) == 0:
+#             return 0 
+
+#         nums.sort() 
+#         longest = 0
+#         curr_length = 1
+
+#         for i in range(1, len(nums)):
+#             if nums[i] == nums[i-1] + 1:
+#                 curr_length += 1
+#             elif nums[i] != nums[i-1]:
+#                 longest = max(longest, curr_length)
+#                 curr_length = 1
+
+#         return max(longest, curr_length)
+        
+#         output=0
 #         if not nums:
 #             return 0
-#         numset=set(nums) #change into set to get unique number only
-
+#         numset=set(nums)
 #         print(numset)
-#         result=1
-#         for num in numset:
-#             count=1  
-#             if num-1 not in numset: #check the left value of in the sets             
-#                 while num+1 in numset: #add+1 right value and check in set
-#                     count+=1       #if yes then count until
-#                     num+=1
-#                     result=max(result,count)
-#         return result 
-                
-# Time O(n)
-# Space O(n)
+#         for n in numset:
+#             if n-1 not in numset:
+#                 start=n
+#                 while start in numset:
+#                     start+=1
+#                 output=max(output,start-n)
+#         return output
+        
