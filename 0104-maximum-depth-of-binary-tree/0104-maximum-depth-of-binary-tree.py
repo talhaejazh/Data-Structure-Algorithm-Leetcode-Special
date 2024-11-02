7d15784b6e46d923a1ee8a9403e559e70d958f1b
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-02 16:58:58 -0400

    Time: 3 ms (31.79%), Space: 18.2 MB (7.40%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -5,56 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-        def maxDepth(self, root):
-            height=[0]
-            def dfs(root):
-                if not root:
-                    return 0
-                lh=dfs(root.left)
-                rh=dfs(root.right)
-                height[0]= 1 + max(lh,rh)
-                return height[0]
-            return dfs(root)
-     
-
-
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        height=[0]
+        def dfs(root):
+            if not root:
+                return 0
+            lh=dfs(root.left)
+            rh=dfs(root.right)
+            height[0]=1 + max(rh,lh)
+            return height[0]
+        return dfs(root)
             
-#             height=[0]
-#             def dfs(root):
-#                 if not root:
-#                         return 0
-#                 lh=dfs(root.left)
-#                 rh=dfs(root.right)
-#                 height[0]=(1+max(lh,rh))
-#                 return height[0]
-#             dfs(root)
-#             return height[0]
-        
             
-#             return dfs(root)
-        
-            #     if not root:
-            #         return 0 
-            #     left,right=1+dfs(root.left),1+dfs(root.right)
-            #     maxLen[0] = max(left,right)
-            #     return maxLen[0]
-            # dfs(root)
-            # return maxLen[0]
-                
-                
-                
-                
-                
-                
-                
-                
-                
-            #     left=1+ dfs(root.left)
-            #     right=1+dfs(root.right)
-            #     maxLen[0]=max(left, right)
-            #     return maxLen[0] 
-            # dfs(root)
-            # return maxLen[0]
-
-
+            
         
