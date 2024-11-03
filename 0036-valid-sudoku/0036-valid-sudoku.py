515dcf1b0e30c9ff0c7c4fc8d9fa5e7bfdc63ca9
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-03 15:35:36 -0500

    Time: 7 ms (39.95%), Space: 16.7 MB (30.55%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,37 +1,37 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # row=[set() for x in range(9)]
-        # col=[set() for y in range(9)]
-        # square=[[set() for x in range(3)] for y in range(3)]
-        # print(square)
-        # for x in range(9):
-        #     for y in range(9):
-        #         cell_value = board[x][y]
-        #         if cell_value=='.':
-        #             continue
-        #         if cell_value in row[x] or cell_value in col[y] or cell_value in square[x//3][y//3]:
-        #             return False
-        #         row[x].add(cell_value)
-        #         col[y].add(cell_value)
-        #         square[x//3][y//3].add(cell_value)
-        #     return True
-        
-        
-        rows = [set() for x in range(9)]
-        columns = [set() for x in range(9)]
-        squares = [[set() for x in range(3)] for y in range(3)]
-        
+        row=[set() for x in range(9)]
+        col=[set() for y in range(9)]
+        square=[[set() for x in range(3)] for y in range(3)]
+        print(square)
         for x in range(9):
             for y in range(9):
                 cell_value = board[x][y]
-                if cell_value == ".":
+                if cell_value=='.':
                     continue
-                if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
+                if cell_value in row[x] or cell_value in col[y] or cell_value in square[x//3][y//3]:
                     return False
-
-                rows[x].add(cell_value)
-                columns[y].add(cell_value)
-                squares[x//3][y//3].add(cell_value)
+                row[x].add(cell_value)
+                col[y].add(cell_value)
+                square[x//3][y//3].add(cell_value)
+        return True
         
-        return True        
+        
+#         rows = [set() for x in range(9)]
+#         columns = [set() for x in range(9)]
+#         squares = [[set() for x in range(3)] for y in range(3)]
+        
+#         for x in range(9):
+#             for y in range(9):
+#                 cell_value = board[x][y]
+#                 if cell_value == ".":
+#                     continue
+#                 if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
+#                     return False
+
+#                 rows[x].add(cell_value)
+#                 columns[y].add(cell_value)
+#                 squares[x//3][y//3].add(cell_value)
+        
+#         return True        
         
