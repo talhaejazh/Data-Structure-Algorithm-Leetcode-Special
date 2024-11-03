889201008cd740ff47dbc9a58e69d60114776500
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-03 15:34:26 -0500

    Time: 0 ms (100.00%), Space: 16.6 MB (30.55%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-#         rows=[set() for x in range(9)]
-#         col=[set() for y in range(9)]
-#         square=[[set() for x in range(3)] for y in range(3)]
-#         print(square)
-#         for x in range(9):
-#             for y in range(9):
-#                 cell_value == board[x][y]
-#                 if cell_value==['.']:
-#                     continue
-#                 if cell_value in row[x] or cell_value in row[y] or cell_value in square[x//3][y//3]:
-#                     return False
-#                 row[x].add(cell_value)
-#                 col[y].add(cell_value)
-#                 square[x//3][y//3].add(cell_value)
-#             return True
+        # row=[set() for x in range(9)]
+        # col=[set() for y in range(9)]
+        # square=[[set() for x in range(3)] for y in range(3)]
+        # print(square)
+        # for x in range(9):
+        #     for y in range(9):
+        #         cell_value = board[x][y]
+        #         if cell_value=='.':
+        #             continue
+        #         if cell_value in row[x] or cell_value in col[y] or cell_value in square[x//3][y//3]:
+        #             return False
+        #         row[x].add(cell_value)
+        #         col[y].add(cell_value)
+        #         square[x//3][y//3].add(cell_value)
+        #     return True
         
         
         rows = [set() for x in range(9)]
