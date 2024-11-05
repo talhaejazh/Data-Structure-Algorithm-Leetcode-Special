98bafca9da9ea1fbfc1de6ff2c54d8cc0861ed3c
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-05 13:18:03 -0500

    Time: 4 ms (61.51%), Space: 16.5 MB (71.09%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,35 +1,35 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # row=[set() for s in range(9)]
-        # col=[set()for y in range(9)]
-        # sqr=[[set()for x in range(3)] for y in range(3)]
-        # print(sqr)
-        # for x in range(9):
-        #     for y in range(9):
-        #         cell_val=board[x][y]
-        #         if cell_val=='.':
-        #             continue
-        #         if cell_val==row[x] or cell_val==col[y] or cell_val==sqr[x//3][y//3]:
-        #             return False
-        #         row[x].add(cell_val)
-        #         col[y].add(cell_val)
-        #         sqr[x//3][y//3].add(cell_val)
-        #     return True
-        
-        
-        row=[set() for x in range(9)]
-        col=[set() for y in range(9)]
-        square=[[set() for x in range(3)] for y in range(3)]
-        print(square)
+        row=[set() for s in range(9)]
+        col=[set()for y in range(9)]
+        sqr=[[set()for x in range(3)] for y in range(3)]
+        print(sqr)
         for x in range(9):
             for y in range(9):
-                cell_value = board[x][y]
-                if cell_value=='.':
+                cell_val=board[x][y]
+                if cell_val=='.':
                     continue
-                if cell_value in row[x] or cell_value in col[y] or cell_value in square[x//3][y//3]: #check condition to see duplicate number in any col row, saquare matrix
+                if cell_val in row[x] or cell_val in col[y] or cell_val in sqr[x//3][y//3]:
                     return False
-                row[x].add(cell_value)
-                col[y].add(cell_value)
-                square[x//3][y//3].add(cell_value)
+                row[x].add(cell_val)
+                col[y].add(cell_val)
+                sqr[x//3][y//3].add(cell_val)
         return True
+        
+        
+        # row=[set() for x in range(9)]
+        # col=[set() for y in range(9)]
+        # square=[[set() for x in range(3)] for y in range(3)]
+        # print(square)
+        # for x in range(9):
+        #     for y in range(9):
+        #         cell_value = board[x][y]
+        #         if cell_value=='.':
+        #             continue
+        #         if cell_value in row[x] or cell_value in col[y] or cell_value in square[x//3][y//3]: #check condition to see duplicate number in any col row, saquare matrix
+        #             return False
+        #         row[x].add(cell_value)
+        #         col[y].add(cell_value)
+        #         square[x//3][y//3].add(cell_value)
+        # return True
         
