5937eef0b86ec15d75c1f6a51bcc74794d6c3da1
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-05 13:17:08 -0500

    Time: 0 ms (100.00%), Space: 16.6 MB (31.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,5 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # row=[set() for s in range(9)]
+        # col=[set()for y in range(9)]
+        # sqr=[[set()for x in range(3)] for y in range(3)]
+        # print(sqr)
+        # for x in range(9):
+        #     for y in range(9):
+        #         cell_val=board[x][y]
+        #         if cell_val=='.':
+        #             continue
+        #         if cell_val==row[x] or cell_val==col[y] or cell_val==sqr[x//3][y//3]:
+        #             return False
+        #         row[x].add(cell_val)
+        #         col[y].add(cell_val)
+        #         sqr[x//3][y//3].add(cell_val)
+        #     return True
+        
+        
         row=[set() for x in range(9)]
         col=[set() for y in range(9)]
         square=[[set() for x in range(3)] for y in range(3)]
@@ -9,29 +26,10 @@ class Solution:
                 cell_value = board[x][y]
                 if cell_value=='.':
                     continue
-                if cell_value in row[x] or cell_value in col[y] or cell_value in square[x//3][y//3]:
+                if cell_value in row[x] or cell_value in col[y] or cell_value in square[x//3][y//3]: #check condition to see duplicate number in any col row, saquare matrix
                     return False
                 row[x].add(cell_value)
                 col[y].add(cell_value)
                 square[x//3][y//3].add(cell_value)
         return True
-        
-        
-#         rows = [set() for x in range(9)]
-#         columns = [set() for x in range(9)]
-#         squares = [[set() for x in range(3)] for y in range(3)]
-        
-#         for x in range(9):
-#             for y in range(9):
-#                 cell_value = board[x][y]
-#                 if cell_value == ".":
-#                     continue
-#                 if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
-#                     return False
-
-#                 rows[x].add(cell_value)
-#                 columns[y].add(cell_value)
-#                 squares[x//3][y//3].add(cell_value)
-        
-#         return True        
         
