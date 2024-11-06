e2ddc927f0789d32df378c2dc6fff07ab2f1f704
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-05 22:04:28 -0500

    Time: 0 ms (100.00%), Space: 17.9 MB (23.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,50 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        numMap={}
-        for i in range(n):
-            complement=target-nums[i]
-            if complement in numMap:
-                return [numMap[complement],i]
-            numMap[nums[i]]=i
-        return []
-        
-        
-        
-        
-        
-#         for i in range(n):
-#             complement=target-nums[i]
-#             if complement in numMap:
-#                 return [numMap[complement],i]
-#             # numMap[nums[i]=i]
-#             numMap[nums[i]] = i
-
-#         return []
-
-        
-        
-        
-        
-        
-        # lookup={}
-        # for i in range(n):
-        #     if nums[i] in lookup:
-        #         return [lookup[nums[i]],i]
-        #     else:
-        #         lookup[target-nums[i]]=i
-        # return None
-
-        
-        
-        
         # n=len(nums)
-        # lookup={}
+        # numMap={}
         # for i in range(n):
-        #     if nums[i] in lookup:
-        #         return [lookup[nums[i]],i]
-        #     else:
-        #         lookup[target-nums[i]]=i
-        # return None
+        #     complement=target-nums[i]
+        #     if complement in numMap:
+        #         return [numMap[complement],i]
+        #     numMap[nums[i]]=i
+        # return []
         
         
+        num_map = {}  # Dictionary to store number and its index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
