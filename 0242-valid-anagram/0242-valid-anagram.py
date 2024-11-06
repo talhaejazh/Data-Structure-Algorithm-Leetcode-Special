52cecd48fce91568aab4907858e3cea2dd935d59
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-05 21:59:10 -0500

    Time: 11 ms (75.60%), Space: 16.6 MB (92.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,36 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
+        if len(s) != len(t):
             return False
-        char_count={}
+        hashmap={}
         for char in s:
-            char_count[char]=char_count.get(char,0)+1
-            
-            # print(char_count)
+            hashmap[char]=hashmap.get(char,0)+1
+            # print(hashmap)
         for char in t:
-            if char not in char_count:
+            if char not in hashmap:
                 return False
-            else:
-                char_count[char]-=1
-            if char_count[char]<0:
+            hashmap[char]-=1
+            if hashmap[char]<0:
                 return False
         return True
+            
+        
+        
+#         if len(s)!=len(t):
+#             return False
+#         char_count={}
+#         for char in s:
+#             char_count[char]=char_count.get(char,0)+1
+            
+#             # print(char_count)
+#         for char in t:
+#             if char not in char_count:
+#                 return False
+#             else:
+#                 char_count[char]-=1
+#             if char_count[char]<0:
+#                 return False
+#         return True
     
             
         
