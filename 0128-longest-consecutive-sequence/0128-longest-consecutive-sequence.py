50d1b396a2cec76994931ae27a87fd0521a3792f
Muhammad Talha Ejaz <70255794+talhaejazh@users.noreply.github.com>
2024-11-05 22:40:47 -0500

    Time: 40 ms (86.70%), Space: 31 MB (93.47%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -13,6 +13,20 @@ class Solution:
                 longest=max(curlen,longest)
                 curlen=1
         return max(curlen,longest)
+        
+        
+#         if len(nums)==0:
+#             return 0
+#         nums.sort()
+#         longest=0
+#         curlen=1
+#         for i in range(1,len(nums)):
+#             if nums[i]==nums[i-1]+1:
+#                 curlen+=1
+#             elif nums[i]!=nums[i-1]:
+#                 longest=max(curlen,longest)
+#                 curlen=1
+#         return max(curlen,longest)
                 
         
         
